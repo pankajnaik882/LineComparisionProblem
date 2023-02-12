c52b7e54a9fbf5bcd3f87c1f015718bdb18faaec
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-12 19:36:58 +0530

    Add : Displaying Welcome to Line Comparison Computation Program

--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     { 
         public static void Main(string[] args) 
         {
-            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            Console.WriteLine("Welcome to Line Comparison Computation Program ");
             LengthOfTwoPoint.lengthOfTwoPoint();
             
         }
