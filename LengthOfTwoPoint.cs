ca42a0dcc6cb4382a2481849e1208edf9745f597
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-12 20:12:45 +0530

    Add : As a fan of geometry, I want to model a line based on a point consisting of (x, y) co -ordinates using the Cartesian system, So that I can calculate its length

---
+++ LengthOfTwoPoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparisionProblem
+{
+    public class LengthOfTwoPoint
+    {
+        public static void lengthOfTwoPoint()
+        {
+            Console.WriteLine("Enter the x1 co-ordinate");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the y1 co-ordinate");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the x2 co-ordinate");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the y2 co-ordinate");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double lengthOfLine = Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+            Console.WriteLine("The lenght of line using co-ordinate : " + lengthOfLine);
+
+
+
+
+        }
+    }
+}
