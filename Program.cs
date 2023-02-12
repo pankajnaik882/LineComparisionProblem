460bca7566289c7f7670b4554f5c8eecfd4bc323
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-12 19:34:01 +0530

    Add : Solving using Switch Case Statement

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace LineComparisionProblem
+{
+    public class LineComparision
+    { 
+        public static void Main(string[] args) 
+        {
+            Console.WriteLine("Welcome to Line Comparison Computation Program");
+            LengthOfTwoPoint.lengthOfTwoPoint();
+            
+        }
+    }
+}
